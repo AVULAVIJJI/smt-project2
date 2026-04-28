aecb24ecff404c4726a2849d9d71bec60746418a
softmastertech <softmastertecch@gmail.com>
2026-04-28 12:34:20 +0530

    yess

--- smt-backend/server.py
+++ smt-backend/server.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:4173",
         "http://127.0.0.1:5173",
+        "https://smt-project2.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
